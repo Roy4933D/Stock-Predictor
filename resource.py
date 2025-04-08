f558b9ebb6aeff627ce1e4d748980d2190d7d54a
Rohan007 <royrohan310303@gmail.com>
2025-04-08 09:10:37 +0530

    Create resource.py

---
+++ resource.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def resources_page():
+    st.title("Resources")
+    st.markdown("""
+    Here are some useful resources to help you get started with investing and stock analysis:
+    
+    ### Articles:
+    - [How to Analyze Stocks](https://example.com/analyze-stocks)
+    - [Beginner's Guide to Investing](https://example.com/beginners-guide)
+    
+    ### Tools:
+    - [Yahoo Finance](https://finance.yahoo.com)
+    - [Google Finance](https://www.google.com/finance)
+    
+    ### Tutorials:
+    - [Stock Market Basics](https://example.com/stock-market-basics)
+    - [Using Machine Learning for Stock Predictions](https://example.com/ml-stock-predictions)
+    """)
